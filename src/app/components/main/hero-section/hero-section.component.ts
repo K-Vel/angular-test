fc7bbd85026a9eba46a859360190bd575a6e4fb4
kate.vel <kate.velll@gmail.com>
2025-05-28 17:36:13 +0200

    feat: added header section

--- src/app/components/main/hero-section/hero-section.component.ts
+++ src/app/components/main/hero-section/hero-section.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.scss'
+  styleUrl: './hero-section.component.scss',
 })
-export class HeroSectionComponent {
-
-}
+export class HeroSectionComponent {}
