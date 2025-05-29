427a061aadbd43a4296b4b6037242ef5e64400e6
kate.vel <kate.velll@gmail.com>
2025-05-29 15:21:33 +0200

    feat: added benefits section

--- src/app/components/main/benefits-section/benefits-section.component.ts
+++ src/app/components/main/benefits-section/benefits-section.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-benefits-section',
-  imports: [],
+  imports: [MatListModule, MatButtonModule],
   templateUrl: './benefits-section.component.html',
-  styleUrl: './benefits-section.component.scss'
+  styleUrl: './benefits-section.component.scss',
 })
-export class BenefitsSectionComponent {
-
-}
+export class BenefitsSectionComponent {}
